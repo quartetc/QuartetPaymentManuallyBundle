120160cfa86f079033b75d7bd2501734af38decc
Shota Hoshino <lga0503@gmail.com>
2014-04-16 18:21:55 +0900

    Fix loading configuration files

--- DependencyInjection/QuartetPaymentManuallyExtension.php
+++ DependencyInjection/QuartetPaymentManuallyExtension.php
@@ -2,8 +2,10 @@
 
 namespace Quartet\Payment\ManuallyBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class QuartetPaymentManuallyExtension extends Extension
 {
@@ -15,5 +17,8 @@ class QuartetPaymentManuallyExtension extends Extension
         $config = $this->processConfiguration($configuration = new Configuration(), $configs);
 
         $container->setAlias('quartet_payment_manually.payment_method', $config['payment_method']);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
